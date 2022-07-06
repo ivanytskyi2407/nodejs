e00d8168cfe040d290fac000b1a4cbbbac278837
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-07-06 17:27:50 +0300

    done

--- contacts.js
+++ contacts.js
@@ -1,3 +1,46 @@
+const { v4 } = require("uuid");
+const path = require("path");
 const fs = require("fs/promises");
+const contactsPath = path.resolve("./db/contacts.json");
 
-fs.readFile();
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contactById = contacts.find((item) => item.id === contactId);
+  if (!contactById) {
+    return null;
+  }
+  return contactById;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const newList = contacts.filter((item) => item.id !== contactId);
+  console.log(newList);
+  fs.writeFile(contactsPath, JSON.stringify(newList));
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+
+  contacts.push({
+    id: v4(),
+    name,
+    email,
+    phone,
+  });
+  console.table(contacts);
+  fs.writeFile(contactsPath, JSON.stringify(contacts));
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,5 +1,47 @@
-const fs = require("fs/promises");
+const contacts = require("./contacts");
+const argv = require("yargs").argv;
 
-fs.readFile("db/contacts.json")
-  .then((data) => console.log(data.toString()))
-  .catch((e) => console.log(e));
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      contacts.listContacts();
+      break;
+
+    case "get":
+      const contact = await contacts.getContactById(id);
+      if (!contact) {
+        throw new Error(`Contact with id=${id} not found`);
+      }
+      console.log(contact);
+      break;
+
+    case "add":
+      const newContact = contacts.addContact(name, email, phone);
+      console.log(newContact);
+      if (!newContact) {
+        throw new Error(`Contact with id=${id} not found`);
+      }
+      console.log(newContact);
+      break;
+
+    case "remove":
+      contacts.removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+invokeAction(argv);
+
+// invokeAction({ action: "list" });
+
+// invokeAction({ action: "get", id: "1" });
+
+// invokeAction({
+//   action: "add",
+//   name: "Oleh Ivanytskyi",
+//   email: "ivanytskyi2407@gmail.com",
+//   phone: "+380676766950",
+// });
+// invokeAction({ action: "remove", id: "1" });
