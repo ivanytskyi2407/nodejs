d3c7379da08a1aeef43f9d84492ea4138d74279e
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-07-08 22:25:28 +0300

    +

--- index.js
+++ index.js
@@ -34,4 +34,6 @@ async function invokeAction({ action, id, name, email, phone }) {
       console.warn("\x1B[31m Unknown action type!");
   }
 }
-invokeAction(argv);
+(async () => {
+  await invokeAction(argv);
+})();
